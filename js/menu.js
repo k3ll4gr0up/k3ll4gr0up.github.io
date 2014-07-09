33f50747d5ef336c6ec65bbbf18e75d8a85697a1
lfuelling <lukas@k40s.net>
2014-07-10 00:42:26 +0200

    animation added

--- js/menu.js
+++ js/menu.js
@@ -9,14 +9,59 @@
 $(document).ready(function() {
 	
 	//initial load
+	_386 = { onePass: true, speedFactor: 1.25 };
+	$(".home").show();
+	$(".about").hide();
+	$(".projects").hide();
+	$(".contact").hide();
+	$("#homeli").attr('class', 'active');
+	$("#aboutli").attr('class', 'inactive');
+	$("#projectsli").attr('class', 'inactive');
+	$("#contactli").attr('class', 'inactive');
 	
-	$("").show();
+	// Menu
 			
 	$('#homea').click(function(){
+					$(".home").show();
+					$(".about").hide();
+					$(".projects").hide();
+					$(".contact").hide();
 					$("#homeli").attr('class', 'active');
 					$("#aboutli").attr('class', 'inactive');
 					$("#projectsli").attr('class', 'inactive');
 					$("#contactli").attr('class', 'inactive');
-				});
+	});
+	$('#abouta').click(function(){
+					$(".home").hide();
+					$(".about").show();
+					$(".projects").hide();
+					$(".contact").hide();
+					$("#homeli").attr('class', 'inactive');
+					$("#aboutli").attr('class', 'active');
+					$("#projectsli").attr('class', 'inactive');
+					$("#contactli").attr('class', 'inactive');
+	});
+	$('#projectsa').click(function(){
+					$(".home").hide();
+					$(".about").hide();
+					$(".projects").show();
+					$(".contact").hide();
+					$("#homeli").attr('class', 'inactive');
+					$("#aboutli").attr('class', 'inactive');
+					$("#projectsli").attr('class', 'active');
+					$("#contactli").attr('class', 'inactive');
+	});
+	$('#contacta').click(function(){
+					$(".home").hide();
+					$(".about").hide();
+					$(".projects").hide();
+					$(".contact").show();
+					$("#homeli").attr('class', 'inactive');
+					$("#aboutli").attr('class', 'inactive');
+					$("#projectsli").attr('class', 'inactive');
+					$("#contactli").attr('class', 'active');
+	});
+				
+				
 			
-			});
+});
