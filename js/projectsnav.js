cb8d587d5851a2bbbf50d250fcbdf795588e907c
lfuelling <lukas@k40s.net>
2014-07-21 09:28:45 +0200

    added veganrecipes

--- js/projectsnav.js
+++ js/projectsnav.js
@@ -12,16 +12,25 @@ $(document).ready(function() {
 				$("#llli").attr('class', 'active');
 				$("#cfli").attr('class', 'inactive');
 				$("#lpli").attr('class', 'inactive');
+				$("#vrli").attr('class', 'inactive');
 			});
 			$('#cfa').click(function(){
 				$("#llli").attr('class', 'inactive');
 				$("#cfli").attr('class', 'active');
 				$("#lpli").attr('class', 'inactive');
+				$("#vrli").attr('class', 'inactive');
 			});
 			$('#lpa').click(function(){
 				$("#llli").attr('class', 'inactive');
 				$("#cfli").attr('class', 'inactive');
 				$("#lpli").attr('class', 'active');
+				$("#vrli").attr('class', 'inactive');
+			});
+			$('#vra').click(function(){
+				$("#llli").attr('class', 'inactive');
+				$("#cfli").attr('class', 'inactive');
+				$("#lpli").attr('class', 'inactive');
+				$("#vrli").attr('class', 'active');
 			});
 
 });
